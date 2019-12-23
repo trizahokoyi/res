b8ddfddb990723e4aca8258a21833bc4eb7b98ad
trizahokoyi <trizahokoyi@gmail.com>
2019-12-22 22:57:15 -0800

    implement changes

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const port = process.env.PORT || 8080
+const path=require("path")
 
 const app = express();
+app.use(express.static("dist"))
 
-app.use("/api", (req, res) => {
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../dist", "index.html"))
+})
+
+app.get("/api", (req, res) => {
   res.send("working!");
 });
 
